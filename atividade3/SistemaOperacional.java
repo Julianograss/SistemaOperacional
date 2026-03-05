ce35b811bf851693e90c0751e2dd7042d499788c
Julianograss <06074736065@senacrs.edu.br>
2026-03-05 20:17:13 -0300

    Refactor login panel layout and add button functionality
    Updated the layout and functionality of the login panel, including adjustments for maximizing the window and added action listener for the 'Entrar' button.

--- atividade3/SistemaOperacional.java
+++ atividade3/SistemaOperacional.java
@@ -12,6 +12,7 @@ public class SistemaOperacional extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+        //painel auxiliar para centralizr o painel de login
         JPanel pnlCentralizar = new JPanel();
         pnlCentralizar.setLayout(new BoxLayout(pnlCentralizar, BoxLayout.Y_AXIS));
         pnlCentralizar.setBackground(Color.GRAY);
@@ -19,10 +20,11 @@ public class SistemaOperacional extends JFrame{
 
         JPanel pnlPainelLogin = new JPanel(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.HORIZONTAL;
         c.insets = new Insets(10, 20, 10, 20);
-        pnlPainelLogin.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-        pnlPainelLogin.setMaximumSize(new Dimension(300, 150));
+        pnlPainelLogin.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        pnlPainelLogin.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        pnlPainelLogin.setMaximumSize(new Dimension(400, 200));
         pnlPainelLogin.setAlignmentX(Component.CENTER_ALIGNMENT);
         pnlPainelLogin.setAlignmentY(Component.CENTER_ALIGNMENT);
         //pnlPainelLogin.setBackground(Color.BLACK);
@@ -35,6 +37,7 @@ public class SistemaOperacional extends JFrame{
         btnEntrar.setAlignmentY(Component.BOTTOM_ALIGNMENT);
         btnEntrar.setMaximumSize(new Dimension(150, 30));
 
+        //ajusta o tamanho do painel de login quando expandir a tela
         addWindowStateListener(new WindowStateListener() {
             @Override
             public void windowStateChanged(WindowEvent e) {
@@ -64,29 +67,76 @@ public class SistemaOperacional extends JFrame{
 
         JPasswordField pswSenhaUsu = new JPasswordField();
 
+        //ajusta a mensagem no GridBagLayout
         c.gridx = 0;
         c.gridy = 0;
         c.gridwidth = 2;
         pnlPainelLogin.add(lblLogMsg, c);
+        c.gridwidth = 1;
+
+        //Usuario
         c.gridx = 0;
         c.gridy = 1;
         pnlPainelLogin.add(lblUsuario, c);
+        c.gridx = 1;
+        c.gridy = 1;
+        c.weightx = 1.0;
+        pnlPainelLogin.add(txtUsuario, c);
+        c.weightx = 0;
+
+        //Senha 
         c.gridx = 0;
         c.gridy = 2;
         pnlPainelLogin.add(lblSenha, c);
         c.gridx = 1;
-        c.gridy = 1;
-        pnlPainelLogin.add(txtUsuario, c);
-        c.gridx = 1;
         c.gridy = 2;
+        c.weightx = 1.0;
         pnlPainelLogin.add(pswSenhaUsu, c);
+        c.weightx = 0;
 
+        //Botao Entrar
+        c.gridx = 0;
+        c.gridy = 3;
+        c.gridwidth = 2;
+        pnlPainelLogin.add(btnEntrar, c);
+        c.gridwidth = 0;
+
+        btnEntrar.addActionListener(e -> {
+            janelaOperador();
+        });
 
 
         pnlCentralizar.add(pnlPainelLogin);
         
         add(pnlCentralizar);
         setVisible(true);
+    }
+    public void janelaOperador(){
+        JFrame jnlOperador = new JFrame("Caixa");
+        jnlOperador.setSize(800,600);
+        jnlOperador.setLocationRelativeTo(null);
+        jnlOperador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jnlOperador.setLayout(new BorderLayout());
+
+        JPanel pnlCentralizar = new JPanel();
+        pnlCentralizar.setBackground(Color.GRAY);
+        pnlCentralizar.setBorder(BorderFactory.createEmptyBorder(150, 0, 0, 0));
+
+        JPanel pnlCaixaStts = new JPanel(new BorderLayout());
+        pnlCaixaStts.setMaximumSize(new Dimension(Integer.MAX_VALUE, 60));
+        pnlCaixaStts.setBorder(BorderFactory.createEmptyBorder(0,10,0,0));
+        pnlCaixaStts.setBackground(Color.BLACK);
+
+        JLabel lblMsgSttsCaix = new JLabel("CAIXA ABERTO");
+        lblMsgSttsCaix.setFont(new Font("Arial", Font.BOLD, 22));
+        lblMsgSttsCaix.setForeground(new Color(57, 255, 20));
+
+
+        jnlOperador.add(pnlCaixaStts, BorderLayout.NORTH);
+        pnlCaixaStts.add(lblMsgSttsCaix, BorderLayout.WEST);
+        jnlOperador.add(pnlCentralizar, BorderLayout.CENTER);
+
+        jnlOperador.setVisible(true);
     }
     public static void main(String args[]){
         new SistemaOperacional();
